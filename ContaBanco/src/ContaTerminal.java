853924e911552054baef9439188876e683331350
Julia <juliarimeniag1@gmail.com>
2023-08-22 22:43:23 -0300

    Projeto

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,9 +1,3 @@
-/*Crie o projeto ContaBanco que receberá dados via terminal contendo as características de conta em banco conforme atributos abaixo:
-Dentro do projeto, crie a classe ContaTerminal.java para realizar toda a codificação do nosso programa.
-Permita que os dados sejam inseridos via terminal sendo que o usuário receberá a mensagem de qual informação será solicitada, exemplo:
-Depois de todas as informações terem sido inseridas, o sistema deverá exibir a seguinte mensagem:
-"Olá [Nome Cliente], obrigado por criar uma conta em nosso banco, sua agência é [Agencia], conta [Numero] e seu saldo [Saldo] já está disponível para saque". */
-
 import java.util.Locale;
 import java.util.Scanner;
 
